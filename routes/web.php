9edb9a2879ec6d6eca884c937e7bbd4a19aaedeb
George Hotten <github@ghott.me>
2024-11-25 14:21:09 +0000

    made rizan's contributions useable in laravel
    - converted the html files to blade
    - moved them to the right place
    - moved the css/js/img to the right place
    - added the pages to the routing file
    - removed redundancy and made the header its own blade file
    - removed redundancy and made the footer its own blade file
    - error 404 page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function() { return view('index'); });
+Route::get('/about', function() { return view('about'); });
+Route::get('/contact', function() { return view('contact'); });
+Route::get('/signup', function() { return view('signup'); });
