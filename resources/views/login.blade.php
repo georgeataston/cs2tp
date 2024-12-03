10b99918b8ecf0e1a8f4942241367c07275c8a97
JoJoWun <159732457+JoJoWun@users.noreply.github.com>
2024-12-03 17:04:24 +0000

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,27 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    
+
     @include("header")
-    
-    <section class="auth-form"><!-- Section for login -->
+
+    <section class="auth-form">
         <div class="auth-form-content">
-            <h2>Login</h2> 
+            <h2>Login to Account</h2>
+            <form action="/login" method="post">
+                @csrf
+                <p class="credentials">Enter email</p>
+                <input type="email" placeholder="Enter email" name="email" required>
 
-    <div class="container">
-        <h1 class="title">Login to Account</h1>
-        <form action="submit" method="post"> 
-            <p class="credentials">Enter email</p>
-            <input type="email" placeholder="Enter email" name="email" required>
+                <p class="credentials">Enter password</p>
+                <input type="password" placeholder="Enter Password" name="psw" required>
 
-            <p class="credentials">Enter password</p>
-            <input type="password" placeholder="Enter Password" name="psw" required>
+                <button type="submit" class="submit-btn">Login</button>
+            </form>
+        </div>
+    </section>
 
-            <button type="submit" class="button">Login</button>
-        </form>
-    </div>
+    @include("footer")
 </body>
 </html>
