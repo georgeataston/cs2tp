7dc5fb8752c851109063c99ec094a196911a8103
George Hotten <github@ghott.me>
2024-12-03 13:13:09 +0000

    added page to routes + fix stylesheet ref
    and added the footer include

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/login.css">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-    
+
     @include("header")
 
     <div class="container">
         <h1 class="title">Login to Account</h1>
-        <form action="submit" method="post"> 
+        <form action="submit" method="post">
             <p class="credentials">Enter email</p>
             <input type="email" placeholder="Enter email" name="email" required>
 
@@ -22,5 +22,7 @@
             <button type="submit" class="button">Login</button>
         </form>
     </div>
+
+    @include("footer")
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@ Route::get('/logout', [AccountController::class, 'invalidateSession']);
 Route::get('/', function() { return view('index'); });
 Route::get('/about', function() { return view('about'); });
 Route::get('/contact', function() { return view('contact'); });
+Route::get('/login', function() { return view('login'); });
 Route::get('/signup', function() { return view('signup'); });
