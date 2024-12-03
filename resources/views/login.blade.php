30167e3ce03a80c2465f067c324e62183ea80c88
JoJoWun <159732457+JoJoWun@users.noreply.github.com>
2024-12-03 02:34:27 +0000

    login page and login css

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="/login.css">
+</head>
+<body>
+    
+    @include("header")
+
+    <div class="container">
+        <h1 class="title">Login to Account</h1>
+        <form action="submit" method="post"> 
+            <p class="credentials">Enter email</p>
+            <input type="email" placeholder="Enter email" name="email" required>
+
+            <p class="credentials">Enter password</p>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit" class="button">Login</button>
+        </form>
+    </div>
+</body>
+</html>
