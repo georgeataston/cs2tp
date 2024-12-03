2a419bf4310799e37e81711e71a2f270f54fb310
JoJoWun <159732457+JoJoWun@users.noreply.github.com>
2024-12-03 17:00:25 +0000

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,15 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="/styles.css">
 </head>
 <body>
-
+    
     @include("header")
+    
+    <section class="auth-form"><!-- Section for login -->
+        <div class="auth-form-content">
+            <h2>Login</h2> 
 
     <div class="container">
         <h1 class="title">Login to Account</h1>
-        <form action="submit" method="post">
+        <form action="submit" method="post"> 
             <p class="credentials">Enter email</p>
             <input type="email" placeholder="Enter email" name="email" required>
 
@@ -22,7 +26,5 @@
             <button type="submit" class="button">Login</button>
         </form>
     </div>
-
-    @include("footer")
 </body>
 </html>
