c41d81f2d070e06a1e03a3f3ea7460201fd6f67d
George Hotten <github@ghott.me>
2024-12-06 11:47:07 +0000

    shop page from db

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,11 @@ Route::get('/contact', function() { return view('contact'); });
 Route::get('/login', function() { return view('login'); })->middleware(ReverseSessionValidator::class);
 Route::get('/signup', function() { return view('signup'); })->middleware(ReverseSessionValidator::class);
 
-Route::get('/shop', function() { return view('shop'); });
+Route::get('/shop', function() {
+    $stockList = Stock::where('quantity', '>', '0')->get();
+
+    return view('shop')->with('stockList', $stockList);
+});
 Route::get('/shop/{id}', function(string $id) {
     if (!is_numeric($id))
         abort('404');
