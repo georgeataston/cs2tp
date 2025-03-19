e0794006e3c4711dfc98635b67c73ecf46ef69f5
ariyana574 <142259963+ariyana574@users.noreply.github.com>
2025-03-19 15:47:00 +0000

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <p>&copy; 2024 - 2025 Crep Culture | <a href="index.html#privacy">Privacy Policy</a> | <a href="index.html#terms">Terms of Service</a></p> <!-- Footer at the bottom of the page-->
+    <p>&copy; 2024 - 2025 Crep Culture | <a href="/footer/privacy-policy">Privacy Policy</a> | <a href="index.html#terms">Terms of Service</a></p> <!-- Footer at the bottom of the page-->
 </footer>
